3d119ad2efe8739ef4f41e19404945e740ba3aa9
cjay <680672+cjay@users.noreply.github.com>
2019-05-16 03:49:36 +0200

    Added MetaResource

---
+++ src/Lib/MetaResource.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+module Lib.MetaResource
+  ( MetaResource
+  , create
+  , destroy
+  , metaResource
+  , auto
+  , manual
+  , alloc
+  , free
+  ) where
+
+import           Graphics.Vulkan.Core_1_0
+import           Lib.Program
+
+-- | drop in replacement for Lib.Program.allocResource
+data MetaResource r a = MetaResource { destroy :: a -> Program' (), create :: Program r a }
+
+metaResource :: (a -> Program' ()) -- ^ destroy resource
+             -> Program r a -- ^ allocate resource
+             -> MetaResource r a
+metaResource destroy create = MetaResource {..}
+{-# INLINE metaResource #-}
+
+auto :: MetaResource r a -> Program r a
+auto MetaResource{..} = allocResource destroy create
+{-# INLINE auto #-}
+
+manual :: MetaResource r a -> Program r (a, Program' ())
+manual MetaResource{..} = do
+  x <- create
+  return (x, destroy x)
+{-# INLINE manual #-}
+
+-- | This is purely for the right wording
+class AllocFree a where
+  free :: MetaResource r a -> (a -> Program' ())
+  free = destroy
+  {-# INLINE free #-}
+
+  alloc :: MetaResource r a -> Program r a
+  alloc = create
+  {-# INLINE alloc #-}
+
+-- These are the only three things that have a vkAlloc.. and vkFree.. function
+instance AllocFree VkCommandBuffer
+instance AllocFree VkDescriptorSet
+instance AllocFree VkDeviceMemory
+instance (AllocFree a) => AllocFree [a]
