576c177a2743afacb943774ee1ba43f05a9c5607
cjay <680672+cjay@users.noreply.github.com>
2020-03-16 16:54:41 +0100

    Device: Prefer discrete GPU and combined gfx/presentation queue

--- src/Vulkyrie/Vulkan/Device.hs
+++ src/Vulkyrie/Vulkan/Device.hs
@@ -12,7 +12,8 @@ module Vulkyrie.Vulkan.Device
 
 import           Control.Monad
 import           Data.Bits
-import           Data.List                            ((\\))
+import           Data.List                            ((\\), sortOn, intersect)
+import           Data.Ord
 import qualified Data.Map                             as Map
 import           Foreign.C.String                     (peekCString)
 import           Graphics.Vulkan
@@ -42,13 +43,20 @@ pickPhysicalDevice vkInstance mVkSurf = do
     when (null devs) $ throwVkMsg "Zero device count!"
     logInfo $ "Found " ++ show (length devs) ++ " devices."
 
-    selectFirstSuitable devs
-  where
-    selectFirstSuitable [] = throwVkMsg "No suitable devices!"
-    selectFirstSuitable (x:xs) = do
-      (mscsd, indeed) <- isDeviceSuitable mVkSurf x
-      if indeed then pure (mscsd, x)
-                else selectFirstSuitable xs
+    suitableDevs <- filterM (fmap snd . isDeviceSuitable mVkSurf) devs
+    when (null suitableDevs) $ throwVkMsg "No suitable devices!"
+    scoredDevs <- mapM (\dev -> deviceScore dev >>= \score -> return (score, dev)) suitableDevs
+    let dev = snd . head $ sortOn (Down . fst) scoredDevs
+    (mscsd, _) <- isDeviceSuitable mVkSurf dev
+    return (mscsd, dev)
+
+
+deviceScore :: VkPhysicalDevice -> Program r Int
+deviceScore pdev = do
+  devProps <- allocaPeek $ \propsPtr ->
+    liftIO $ vkGetPhysicalDeviceProperties pdev propsPtr
+  let deviceType = getField @"deviceType" devProps
+  if deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU then return 100 else return 0
 
 
 querySwapchainSupport :: VkPhysicalDevice
@@ -148,33 +156,19 @@ getQueueFamilies pdev = do
   return $ zip [0..] fams
 
 
--- | Throw an error otherwise
-selectGraphicsFamily :: [(Word32, VkQueueFamilyProperties)]
-                     -> Program r (Word32, VkQueueFamilyProperties)
-selectGraphicsFamily []
-  = throwVkMsg "selectGraphicsFamily: not found!"
-selectGraphicsFamily (x@(_,qfp):xs)
-  = if  getField @"queueCount" qfp > 0
-     && getField @"queueFlags" qfp .&. VK_QUEUE_GRAPHICS_BIT /= zeroBits
-    then pure x
-    else selectGraphicsFamily xs
+isGraphicsFam :: (Word32, VkQueueFamilyProperties) -> Bool
+isGraphicsFam (_, qfp)
+  = getField @"queueFlags" qfp .&. VK_QUEUE_GRAPHICS_BIT /= zeroBits
 
-
--- | Throw an error otherwise
-selectPresentationFamily :: VkPhysicalDevice
-                         -> VkSurfaceKHR
-                         -> [(Word32, VkQueueFamilyProperties)]
-                         -> Program r (Word32, VkQueueFamilyProperties)
-selectPresentationFamily _ _ []
-  = throwVkMsg "selectPresentationFamily: not found!"
-selectPresentationFamily dev surf (x@(i,qfp):xs)
-  | getField @"queueCount" qfp <= 0 = selectGraphicsFamily xs
-  | otherwise = do
+isPresentationFam :: VkPhysicalDevice
+                  -> VkSurfaceKHR
+                  -> (Word32, VkQueueFamilyProperties)
+                  -> Program r Bool
+isPresentationFam dev surf (i, _)
+  = do
     supported <- allocaPeek $
       runVk . vkGetPhysicalDeviceSurfaceSupportKHR dev i surf
-    if supported == VK_TRUE
-    then pure x
-    else selectPresentationFamily dev surf xs
+    return (supported == VK_TRUE)
 
 
 data DevQueues
@@ -196,19 +190,27 @@ createGraphicsDevice pdev surf
 
   -- find an appropriate queue family
   qfams <- getQueueFamilies pdev
-  (gFamIdx, _gFam) <- selectGraphicsFamily qfams
-  (pFamIdx, _pFam) <- selectPresentationFamily pdev surf qfams
-  let qFamIndices = Map.fromList [(gFamIdx, gFamIdx), (pFamIdx, pFamIdx)]
-  famIndsPtr <- newArrayRes $ Map.elems qFamIndices
+  let gfxFams = map fst $ filter isGraphicsFam qfams
+  when (null gfxFams) $ throwVkMsg "No graphics queue family found."
+  presFams <- map fst <$> filterM (isPresentationFam pdev surf) qfams
+  when (null presFams) $ throwVkMsg "No presentation queue family found for the surface."
+  -- It's best for performance if presentation can happen in the graphics queue
+  let bestFams = gfxFams `intersect` presFams
+  let (gfxFamIdx, presFamIdx) =
+        case bestFams of
+          fam:_ -> (fam, fam)
+          [] -> (head gfxFams, head presFams)
+      qFamIndices = if gfxFamIdx == presFamIdx then [gfxFamIdx] else [gfxFamIdx, presFamIdx]
+  famIndsPtr <- newArrayRes qFamIndices
 
-  let qcInfoMap = flip fmap qFamIndices $ \qFamIdx ->
-               createVk @VkDeviceQueueCreateInfo
-        $  set @"sType" VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO
-        &* set @"pNext" VK_NULL
-        &* set @"flags" VK_ZERO_FLAGS
-        &* set @"queueFamilyIndex" qFamIdx
-        &* set @"queueCount" 1
-        &* setListRef @"pQueuePriorities" [1.0]
+  let qcInfoMap = Map.fromList $ flip fmap qFamIndices $ \qFamIdx -> (qFamIdx, qCreateInfo qFamIdx)
+        where qCreateInfo qFamIdx = createVk @VkDeviceQueueCreateInfo
+                $  set @"sType" VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO
+                &* set @"pNext" VK_NULL
+                &* set @"flags" VK_ZERO_FLAGS
+                &* set @"queueFamilyIndex" qFamIdx
+                &* set @"queueCount" 1
+                &* setListRef @"pQueuePriorities" [1.0]
 
       pdevFeatures = createVk @VkPhysicalDeviceFeatures
         $  set @"samplerAnisotropy" VK_TRUE
@@ -237,10 +239,10 @@ createGraphicsDevice pdev surf
 
   mdevQueues <- maybe (throwVkMsg "Some queues lost!") pure
                 $ DevQueues
-               <$> Map.lookup gFamIdx gQueues
-               <*> Map.lookup pFamIdx gQueues
+               <$> Map.lookup gfxFamIdx gQueues
+               <*> Map.lookup presFamIdx gQueues
                <*> Just famIndsPtr
-               <*> Just gFamIdx
-               <*> Just pFamIdx
+               <*> Just gfxFamIdx
+               <*> Just presFamIdx
 
   return (dev, mdevQueues)
