cjay/vulkyrie

commit edbd0fe05e9550e50784b5a20b899933a9b6cb68

Resource ownership transfer and various fixes
- Program: Abolished occupyThreadAndFork
- GLFW: Reimplemented glfwWaitEventsMeanwhile that used
  occupyThreadAndFork, now with threadRes
- Concurrent: OwnedThread now throws exceptions to the owning thread instead
  of the creating thread
- Resource: Implemented ResourceOwner and taking ownership from it
- Draw: Bracket over swapchainVar
- Draw, Main, Presentation: Abolished swapchainSlot, this was a kludge
  for restricted resource management. Replaced by ResourceOwner usage.

src/Vulkyrie/Engine/Draw.hs
@@ -38,7 +38,7 @@ type RenderFun =
 
 data RenderData
   = RenderData
-  { swapchainVar              :: MVar (Maybe VkSwapchainKHR)
+  { swapchainVar              :: MVar VkSwapchainKHR
     -- ^ Swapchain in MVar to synchronize host access for vkAcquireNextImageKHR
   , presentQueue              :: ManagedPresentQueue
     -- ^ Presentation queue. Not necessarily the same as the graphics queue(s).
@@ -65,25 +65,25 @@ drawFrame EngineCapability{ dev, semPool, cmdQueue } RenderData{..} = do
     imageAvailSem <- head <$> acquireSemaphores semPool 1
 
     liftIO $ acquireToken swapImgTokens
-    swapchain <- takeMVar swapchainVar >>= \case
-      Just sc -> return sc
-      Nothing -> error "unexpected Nothing in swapchain slot"
-    -- Acquiring an image from the swapchain
-    -- Can throw VK_ERROR_OUT_OF_DATE_KHR
-    -- TODO: validation layer complaint about too many acquired images is
-    -- probably not justified, waiting for vulkan spec to be clarified
-    imgIndex <- allocaPeek $ \imgIndexPtr -> runAndCatchVk
-      ( vkAcquireNextImageKHR
-            dev swapchain maxBound
-            imageAvailSem VK_NULL_HANDLE imgIndexPtr
+    imgIndex <- bracket
+      (takeMVar swapchainVar)
+      (putMVar swapchainVar)
+      (\swapchain ->
+        -- Acquiring an image from the swapchain
+        -- Can throw VK_ERROR_OUT_OF_DATE_KHR
+        -- TODO: validation layer complaint about too many acquired images is
+        -- probably not justified, waiting for vulkan spec to be clarified
+        allocaPeek $ \imgIndexPtr -> runAndCatchVk
+          ( vkAcquireNextImageKHR
+                dev swapchain maxBound
+                imageAvailSem VK_NULL_HANDLE imgIndexPtr
+          )
+          ( \(err :: VulkanException) -> do
+              releaseSemaphores semPool [imageAvailSem]
+              liftIO $ releaseToken swapImgTokens
+              throwIO err
+          )
       )
-      ( \(err :: VulkanException) -> do
-          releaseSemaphores semPool [imageAvailSem]
-          putMVar swapchainVar (Just swapchain)
-          liftIO $ releaseToken swapImgTokens
-          throwIO err
-      )
-    putMVar swapchainVar (Just swapchain)
 
     nextSems_ <- takeMVar nextSems
     putMVar nextSems []

src/Vulkyrie/Engine/Main.hs
@@ -114,9 +114,12 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
     beforeSwapchainCreation
     scsd <- querySwapchainSupport pdev vulkanSurface
 
-    nextSwapchainSlot <- auto $ createSwapchainSlot dev
-    (firstSwapchain, firstSwapInfo) <- createSwapchain dev scsd queues vulkanSurface syncMode Nothing
-    putMVar nextSwapchainSlot (Just firstSwapchain)
+    swapchainOwner <- auto resourceOwner
+    (firstSwapchain, firstSwapInfo) <-
+      withResourceOwner swapchainOwner $
+        auto $ createSwapchain dev scsd queues vulkanSurface syncMode Nothing
+    -- The MVar is used to hold the current swapchain and to synchronize access to it
+    swapchainVar <- newMVar firstSwapchain
     swapInfoRef <- newIORef firstSwapInfo
     swapImgTokens <- liftIO . newNatTokenVar $ swapMaxAcquired firstSwapInfo
 
@@ -129,9 +132,9 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
     -- The code below re-runs when the swapchain was re-created
     asyncRedo $ \redoWithNewSwapchain -> region $ do
       logInfo "New thread: Creating things that depend on the swapchain.."
-      -- need this for delayed destruction of the old swapchain if it gets replaced
-      swapchainSlot <- auto $ createSwapchainSlot dev
-      putMVar swapchainSlot =<< takeMVar nextSwapchainSlot
+      -- Need this for deferred destruction of the swapchain if it gets replaced
+      -- by a new one
+      takeOwnership swapchainOwner
       swapInfo <- readIORef swapInfoRef
       -- TODO waiting for vulkan spec clarification: acquiredness of images after swapchain replacement
       liftIO $ changeNumTokens swapImgTokens (swapMaxAcquired swapInfo)
@@ -146,7 +149,7 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
 
       shouldExit <- glfwMainLoop window $ do
         let rdata = RenderData
-              { swapchainVar = swapchainSlot
+              { swapchainVar
               , presentQueue
               , swapImgTokens
               , renderFinishedSems
@@ -182,13 +185,14 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
           then do
             beforeSwapchainCreation
             logInfo "Recreating swapchain.."
-            swapchain <- takeMVar swapchainSlot
+            swapchain <- takeMVar swapchainVar
             -- This query should happen as close to createSwapchain as possible to
             -- get the latest changes to the window size.
             newScsd <- querySwapchainSupport pdev vulkanSurface
-            (newSwapchain, newSwapInfo) <- createSwapchain dev newScsd queues vulkanSurface syncMode swapchain
-            putMVar swapchainSlot swapchain
-            putMVar nextSwapchainSlot (Just newSwapchain)
+            (newSwapchain, newSwapInfo) <-
+              withResourceOwner swapchainOwner $
+                auto $ createSwapchain dev newScsd queues vulkanSurface syncMode (Just swapchain)
+            putMVar swapchainVar newSwapchain
             atomicWriteIORef swapInfoRef newSwapInfo
             redoWithNewSwapchain
             return $ AbortLoop ()

src/Vulkyrie/Program.hs
@@ -24,7 +24,6 @@ module Vulkyrie.Program
       -- * Other
     , LoopControl (..)
     , loop
-    , occupyThreadAndFork
     , touchIORef
     ) where
 
@@ -40,8 +39,6 @@ import           Control.Monad.Trans.Reader (ReaderT, runReaderT)
 import           Data.Text
 import           GHC.Stack ( HasCallStack, callStack )
 import           Graphics.Vulkan.Core_1_0 ( VkResult(VK_SUCCESS) )
-import           System.Exit
-import           UnliftIO.Concurrent
 import           UnliftIO.Exception
 import           UnliftIO.IORef
 
@@ -127,22 +124,6 @@ loop action = go
   where go = action >>= \case
           ContinueLoop -> go
           AbortLoop a -> return a
-
-
--- | For C functions that have to run in the main thread as long as the program runs.
---
---   Caveat: The separate thread is not a bound thread, in contrast to the main thread.
---   Use `runInBoundThread` there if you need thread local state for C libs.
-occupyThreadAndFork :: Prog r () -- ^ the program to run in the main thread
-                    -> Prog r () -- ^ the program to run in a separate thread
-                    -> Prog r ()
-occupyThreadAndFork mainProg deputyProg = do
-  mainThreadId <- myThreadId
-  -- TODO proper thread management. at least wrap in some AsyncException.
-  void $ forkFinally deputyProg $ \case
-    Left exception -> throwTo mainThreadId exception
-    Right ()       -> throwTo mainThreadId ExitSuccess
-  mainProg
 
 
 -- | to make sure IORef writes arrive in other threads

src/Vulkyrie/Vulkan/Queue.hs
@@ -62,22 +62,19 @@ data ManagedPresentQueue =
     -- ^ needed because there is no single management thread for the queue
   }
 
-present :: ManagedPresentQueue -> [(MVar (Maybe VkSwapchainKHR), Word32)] -> [VkSemaphore] -> Prog r ()
+present :: ManagedPresentQueue -> [(MVar VkSwapchainKHR, Word32)] -> [VkSemaphore] -> Prog r ()
 present ManagedPresentQueue{..} images waitSems = do
   let (swapchainVars, imageIndices) = unzip images
   bracket
     (mapM takeMVar swapchainVars)
     (\swapchains -> sequence_ $ putMVar <$> ZipList swapchainVars <*> ZipList swapchains)
     (\swapchains -> do
-      actualSwapchains <- forM swapchains $ \case
-        Just sc -> return sc
-        Nothing -> error "unexpected Nothing in swapchain slot"
       let presentInfo = createVk @VkPresentInfoKHR
             $  set @"sType" VK_STRUCTURE_TYPE_PRESENT_INFO_KHR
             &* set @"pNext" VK_NULL
             &* setListRef @"pImageIndices" imageIndices
             &* setListCountAndRef @"waitSemaphoreCount" @"pWaitSemaphores" waitSems
-            &* setListCountAndRef @"swapchainCount" @"pSwapchains" actualSwapchains
+            &* setListCountAndRef @"swapchainCount" @"pSwapchains" swapchains
       withVkPtr presentInfo $ \ptr -> runAndCatchVk
         (vkQueuePresentKHR presentQueue ptr)
         ( \exception@(VulkanException ecode) ->

src/Vulkyrie/Concurrent.hs
@@ -67,14 +67,14 @@ threadRes prog = Resource $ do
                 "uncaught exception in a thread\n\nthread defined at: "
                   <> threadSrc <> "\n\n" <> showt e
               throwTo parentThreadId (ChildThreadException threadSrc e)
-      -- alive only gets written in one spot, no race conditions possible
-      writeIORef alive False
+      atomicWriteIORef alive False
     )
 
 data ThreadOwner =
   ThreadOwner
   -- MVar to allow threads to remove themselves
-  { threadsVar :: MVar (Set ThreadId) -- ^ keeps track of alive threads
+  { owningThreadId :: ThreadId
+  , threadsVar :: MVar (Set ThreadId) -- ^ keeps track of alive threads
   }
 
 -- | A  Resource that owns threads and limits their lifetime to the region.
@@ -86,6 +86,7 @@ data ThreadOwner =
 threadOwner :: HasCallStack => Resource ThreadOwner
 threadOwner = Resource $ do
   let ownerSrc = pack $ prettyCallStack callStack
+  owningThreadId <- myThreadId
   autoDestroyCreate
     (\ThreadOwner{ threadsVar } -> do
       threads <- takeMVar threadsVar
@@ -95,15 +96,14 @@ threadOwner = Resource $ do
             <> ownerSrc
       forM_ threads killThread
     )
-    (ThreadOwner <$> newMVar Set.empty)
+    (ThreadOwner owningThreadId <$> newMVar Set.empty)
 
 -- | Make a new thread owned by the ThreadOwner.
 --
 -- Make sure to only use this while the region that contains the thread owner is
 -- still alive.
 ownedThread :: HasCallStack => ThreadOwner -> (forall r. Prog r ()) -> Prog r' ThreadId
-ownedThread ThreadOwner{ threadsVar } prog = do
-  parentThreadId <- myThreadId
+ownedThread ThreadOwner{ owningThreadId, threadsVar } prog = do
   let threadSrc = pack $ prettyCallStack callStack
   mask_ $ do
     threads <- takeMVar threadsVar
@@ -112,7 +112,7 @@ ownedThread ThreadOwner{ threadsVar } prog = do
       unmask prog `catchAsync` \(e :: SomeException) ->
         unless (fromException e == Just ThreadKilled) $
           case fromException e of
-            Just (_ :: ChildThreadException) -> throwTo parentThreadId e
+            Just (_ :: ChildThreadException) -> throwTo owningThreadId e
             _ -> do
               -- Logging here just in case the recursive destruction of
               -- resources gets stuck somewhere or the user aborts the program
@@ -120,7 +120,7 @@ ownedThread ThreadOwner{ threadsVar } prog = do
               logErrorN $
                 "uncaught exception in a thread\n\nthread defined at: "
                   <> threadSrc <> "\n\n" <> showt e
-              throwTo parentThreadId (ChildThreadException threadSrc e)
+              throwTo owningThreadId (ChildThreadException threadSrc e)
       modifyMVar_ threadsVar $ pure . Set.delete threadId
     putMVar threadsVar $ Set.insert threadId threads
     pure threadId
@@ -132,6 +132,10 @@ data RedoSignal = SigRedo | SigExit deriving Eq
 --
 -- The argument action gets passed a redo trigger that causes the action to be
 -- restarted in a new thread while the old one is still running.
+-- The function blocks until the last redo has finished. Threads for older redo
+-- action that are still alive when the last redo has finished get killed at
+-- that point, though if they are stuck on destructors they will stay that way
+-- because of exception masking around the destructors.
 asyncRedo :: (forall r. (forall s. Prog s ()) -> Prog r ()) -> Prog t ()
 asyncRedo prog = region $ do
   owner <- auto threadOwner

src/Vulkyrie/GLFW.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Strict #-}
+{-# LANGUAGE RankNTypes #-}
 module Vulkyrie.GLFW
     ( createGLFWVulkanInstance
     , initGLFWWindow
@@ -9,18 +10,19 @@ module Vulkyrie.GLFW
     ) where
 
 import           Control.Applicative
-import           Control.Monad       (forever, unless, when)
+import           Control.Monad
 import           Control.Monad.Trans.Maybe
+import           Data.Text (pack)
 import           Graphics.UI.GLFW    (ClientAPI (..), WindowHint (..))
 import qualified Graphics.UI.GLFW    as GLFW
 import           Graphics.Vulkan
 import           UnliftIO.Exception
 import           UnliftIO.IORef
 
+import           Vulkyrie.Concurrent
 import           Vulkyrie.Program
 import           Vulkyrie.Resource
 import           Vulkyrie.Vulkan.Instance
-import Data.Text (pack)
 
 
 initGLFWWindow :: Int -- ^ Window width. Ignored if fullscreen.
@@ -87,11 +89,17 @@ glfwMainLoop w action = go
 --   Waits repeatedly with 1 second timeout to allow exceptions to be handled
 --   without events happening. If waiting without timeout, the waitEvents
 --   function would need to be marked interruptible in the GLFW binding.
-glfwWaitEventsMeanwhile :: IO () -> Prog r () -> Prog r ()
-glfwWaitEventsMeanwhile mainThreadHook =
-  occupyThreadAndFork
-    (liftIO $ forever $ GLFW.waitEventsTimeout 1 >> mainThreadHook)
-
+--
+--   Caveat: The separate thread is not a bound thread, in contrast to the main thread.
+--   Use `runInBoundThread` there if you need thread local state for C libs.
+glfwWaitEventsMeanwhile :: IO () -> (forall r. Prog r ()) -> Prog s ()
+glfwWaitEventsMeanwhile mainThreadHook prog = region $ do
+  doneVar <- newIORef False
+  void . auto $ threadRes (prog >> atomicWriteIORef doneVar True)
+  loop $ do
+    liftIO $ GLFW.waitEventsTimeout 1 >> mainThreadHook
+    done <- readIORef doneVar
+    return $ if done then AbortLoop () else ContinueLoop
 
 glfwWaitMinimized :: GLFW.Window -> Prog r ()
 glfwWaitMinimized win = liftIO go where

commit ac3f3516bc87ef93a619431380ecf4b2111f92ab

Swapchain fixes
- Fixed validation layer complaint about too many acquired images,
  related to not well defined swapchain image acquiredness from the
  Vulkan specification. This is on the safe side now, no matter the
  spec problems.
- Fixed regression from edbd0fe05e9550e50784b5a20b899933a9b6cb68: Tried
  to present images of old swapchain with new swapchain

src/Vulkyrie/Engine/Draw.hs
@@ -22,6 +22,7 @@ import           Vulkyrie.Utils
 import           Vulkyrie.Vulkan.Engine
 import           Vulkyrie.Vulkan.Queue
 import           Vulkyrie.Vulkan.Sync
+import UnliftIO.IORef
 
 -- | Callback type for rendering into swapchain framebuffers.
 --
@@ -40,6 +41,8 @@ data RenderData
   = RenderData
   { swapchainVar              :: MVar VkSwapchainKHR
     -- ^ Swapchain in MVar to synchronize host access for vkAcquireNextImageKHR
+  , swapOutdated              :: IORef Bool
+    -- ^ Gets set to true if presentation faild with VK_ERROR_OUT_OF_DATE_KHR
   , presentQueue              :: ManagedPresentQueue
     -- ^ Presentation queue. Not necessarily the same as the graphics queue(s).
   , swapImgTokens             :: NatTokenVar
@@ -65,15 +68,25 @@ drawFrame EngineCapability{ dev, semPool, cmdQueue } RenderData{..} = do
     imageAvailSem <- head <$> acquireSemaphores semPool 1
 
     liftIO $ acquireToken swapImgTokens
-    imgIndex <- bracket
-      (takeMVar swapchainVar)
-      (putMVar swapchainVar)
+    -- A present that failed probably doesn't unacquire the image, for that
+    -- reason we HAVE to bail out here, as the vkAcquireNextImageKHR would
+    -- probably be an API violation if too many images are acquired, and indeed
+    -- the validation layers complain when that happens. "probably", because
+    -- acquiredness is not very well defined in the Vulkan spec.
+    -- See also:
+    -- - https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/1696
+    -- - https://github.com/KhronosGroup/Vulkan-Docs/issues/1242
+    readIORef swapOutdated >>= flip when
+      (do
+        releaseSemaphores semPool [imageAvailSem]
+        liftIO $ releaseToken swapImgTokens
+        throwIO $ VulkanException VK_ERROR_OUT_OF_DATE_KHR
+      )
+    (imgIndex, status) <- withMVar swapchainVar
       (\swapchain ->
         -- Acquiring an image from the swapchain
         -- Can throw VK_ERROR_OUT_OF_DATE_KHR
-        -- TODO: validation layer complaint about too many acquired images is
-        -- probably not justified, waiting for vulkan spec to be clarified
-        allocaPeek $ \imgIndexPtr -> runAndCatchVk
+        allocaPeekRet $ \imgIndexPtr -> runAndCatchVk
           ( vkAcquireNextImageKHR
                 dev swapchain maxBound
                 imageAvailSem VK_NULL_HANDLE imgIndexPtr
@@ -99,14 +112,29 @@ drawFrame EngineCapability{ dev, semPool, cmdQueue } RenderData{..} = do
 
     -- VK_ERROR_OUT_OF_DATE_KHR is supressed when presenting here, because ManagedQueue can't do anything with that
     let presentAction = present presentQueue [(swapchainVar, imgIndex)] [renderFinishedSem]
-    submitPresent cmdQueue presentQueue presentAction (liftIO $ releaseToken swapImgTokens)
+    submitPresent cmdQueue presentQueue presentAction $ \result -> do
+      if result < VK_SUCCESS && result /= VK_ERROR_OUT_OF_DATE_KHR then
+        throwIO $ VulkanException result
+      else do
+        -- The token release is kind of fake when out-of-date, but needed to
+        -- avoid deadlock in the next drawFrame call. The swapOutdated update
+        -- then tells that call what is going on.
+
+        -- TODO: It would be better to throw an exception at the asyncRedo
+        -- thread to abort things asap, but that needs a redesign of the
+        -- swapchain handling, and has to wait until everything is resourcified
+        -- to handle async exceptions gracefully.
+        when (result == VK_ERROR_OUT_OF_DATE_KHR) $ atomicWriteIORef swapOutdated True
+        liftIO $ releaseToken swapImgTokens
 
     void $ forkIO $ do
       waitDone nextEvent
       writeChan renderFinishedSems renderFinishedSem
       liftIO $ Event.signal frameFinishedEvent
 
-    -- TODO vkGetSwapchainStatusKHR not found, bug in vulkan-api?
+    -- TODO It would be nice to get a fresher swapchain status here, but
+    -- vkGetSwapchainStatusKHR not found, bug in vulkan-api?
     -- result <- runVkResult $ vkGetSwapchainStatusKHR dev swapchain
-    -- return (result == VK_SUBOPTIMAL_KHR)
-    return False
+
+    -- if it's VK_ERROR_OUT_OF_DATE_KHR instead, an exception will be thrown above
+    return (status == VK_SUBOPTIMAL_KHR)

src/Vulkyrie/Engine/Main.hs
@@ -119,7 +119,7 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
       withResourceOwner swapchainOwner $
         auto $ createSwapchain dev scsd queues vulkanSurface syncMode Nothing
     -- The MVar is used to hold the current swapchain and to synchronize access to it
-    swapchainVar <- newMVar firstSwapchain
+    nextSwapchainVar <- newMVar firstSwapchain
     swapInfoRef <- newIORef firstSwapInfo
     swapImgTokens <- liftIO . newNatTokenVar $ swapMaxAcquired firstSwapInfo
 
@@ -135,8 +135,16 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
       -- Need this for deferred destruction of the swapchain if it gets replaced
       -- by a new one
       takeOwnership swapchainOwner
+      swapchainVar <- newEmptyMVar
+      putMVar swapchainVar =<< takeMVar nextSwapchainVar
       swapInfo <- readIORef swapInfoRef
-      -- TODO waiting for vulkan spec clarification: acquiredness of images after swapchain replacement
+      swapOutdated <- newIORef False
+      -- TODO Waiting for vulkan spec clarification: acquiredness of images after swapchain replacement.
+      -- See also the notes in the Draw module. It should be very beneficial for
+      -- performance to replace the token var by a fresh one right away when the
+      -- old swapchain caused out-of-date errors, but I'm not sure the spec
+      -- allows for that. Maybe in that case the old swapchain should not be
+      -- passed when creating the new one, then it should be ok.
       liftIO $ changeNumTokens swapImgTokens (swapMaxAcquired swapInfo)
 
       (framebuffers, nextAppSems) <- auto $ appNewSwapchain appState swapInfo
@@ -150,6 +158,7 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
       shouldExit <- glfwMainLoop window $ do
         let rdata = RenderData
               { swapchainVar
+              , swapOutdated
               , presentQueue
               , swapImgTokens
               , renderFinishedSems
@@ -185,14 +194,13 @@ runVulkanProgram App{ .. } = runProgram $ region $ do
           then do
             beforeSwapchainCreation
             logInfo "Recreating swapchain.."
-            swapchain <- takeMVar swapchainVar
-            -- This query should happen as close to createSwapchain as possible to
-            -- get the latest changes to the window size.
-            newScsd <- querySwapchainSupport pdev vulkanSurface
-            (newSwapchain, newSwapInfo) <-
+            (newSwapchain, newSwapInfo) <- withMVar swapchainVar $ \swapchain -> do
+              -- This query should happen as close to createSwapchain as possible to
+              -- get the latest changes to the window size.
+              newScsd <- querySwapchainSupport pdev vulkanSurface
               withResourceOwner swapchainOwner $
                 auto $ createSwapchain dev newScsd queues vulkanSurface syncMode (Just swapchain)
-            putMVar swapchainVar newSwapchain
+            putMVar nextSwapchainVar newSwapchain
             atomicWriteIORef swapInfoRef newSwapInfo
             redoWithNewSwapchain
             return $ AbortLoop ()

src/Vulkyrie/Program.hs
@@ -127,10 +127,11 @@ runVkResult action = do
 
 -- | Calls handler directly for error VkResult codes.
 -- TODO rewrite rule?
-runAndCatchVk :: MonadIO m => IO VkResult -> (VulkanException -> m ()) -> m ()
+runAndCatchVk :: MonadIO m => IO VkResult -> (VulkanException -> m ()) -> m VkResult
 runAndCatchVk action handler = do
   r <- liftIO action
   when (r < VK_SUCCESS) (handler $ VulkanException r)
+  pure r
 
 
 

src/Vulkyrie/Program/Foreign.hs
@@ -14,7 +14,7 @@ module Vulkyrie.Program.Foreign
     , peek, peekArray, poke
     , ptrAtIndex
     , asListVk
-    , allocaPeek, allocaPeekVk, allocaPeekDF
+    , allocaPeek, allocaPeekRet, allocaPeekVk, allocaPeekDF
     , mallocRes, mallocArrayRes, newArrayRes
     , withUnsafeField
     , getListCountAndRef
@@ -108,6 +108,15 @@ allocaPeek :: Storable a
 allocaPeek f = alloca $ \ptr -> f ptr >> liftIO (Storable.peek ptr)
 {-# INLINE allocaPeek #-}
 
+allocaPeekRet :: Storable a
+              => (Ptr a -> Prog r b)
+              -> Prog r (a, b)
+allocaPeekRet f =
+  alloca $ \ptr -> do
+    result <- f ptr
+    content <- liftIO (Storable.peek ptr)
+    pure (content, result)
+{-# INLINE allocaPeekRet #-}
 
 peekArray :: Storable a => Int -> Ptr a -> Prog r [a]
 peekArray = Foreign.peekArray

src/Vulkyrie/Vulkan/Queue.hs
@@ -62,7 +62,7 @@ data ManagedPresentQueue =
     -- ^ needed because there is no single management thread for the queue
   }
 
-present :: ManagedPresentQueue -> [(MVar VkSwapchainKHR, Word32)] -> [VkSemaphore] -> Prog r ()
+present :: ManagedPresentQueue -> [(MVar VkSwapchainKHR, Word32)] -> [VkSemaphore] -> Prog r VkResult
 present ManagedPresentQueue{..} images waitSems = do
   let (swapchainVars, imageIndices) = unzip images
   bracket
@@ -75,15 +75,9 @@ present ManagedPresentQueue{..} images waitSems = do
             &* setListRef @"pImageIndices" imageIndices
             &* setListCountAndRef @"waitSemaphoreCount" @"pWaitSemaphores" waitSems
             &* setListCountAndRef @"swapchainCount" @"pSwapchains" swapchains
-      withVkPtr presentInfo $ \ptr -> runAndCatchVk
+      -- not doing runVk here to let the postPresentAction handle the swapchain status
+      withVkPtr presentInfo $ \ptr -> liftIO
         (vkQueuePresentKHR presentQueue ptr)
-        ( \exception@(VulkanException ecode) ->
-            case ecode of
-              -- Ignoring this here is ok, vkAcquireNextImageKHR or
-              -- vkGetSwapchainStatusKHR will throw it again later.
-              VK_ERROR_OUT_OF_DATE_KHR -> return ()
-              _ -> throwIO exception
-        )
     )
 
 
@@ -162,7 +156,7 @@ data QueueRequest = Shutdown
 data Instruction = Post VkSubmitInfo
                  | SubmitIfNeeded
                  | Submit
-                 | Present ManagedPresentQueue (forall r. Prog r ()) (IO ())
+                 | Present ManagedPresentQueue (forall r. Prog r VkResult) (forall r. VkResult -> Prog r ())
                  | Notify (MVar QueueEvent)
                  | Checkpoint Event
 
@@ -229,13 +223,13 @@ metaManagedQueue dev queue msp =
                   Checkpoint event -> liftIO $ Event.set event
                   Present (ManagedPresentQueue presentQueue mutex) presentAction postPresentAction ->
                     if presentQueue == queue
-                      then presentAction >> liftIO postPresentAction
+                      then presentAction >>= postPresentAction
                       else void $ forkIO $ do
                         -- TODO exception masking. Depends on how to handle actual async exceptions within Program
                         takeMVar mutex
-                        presentAction
+                        result <- presentAction
                         putMVar mutex ()
-                        liftIO postPresentAction
+                        postPresentAction result
                 queueLoop
               Shutdown -> return ()
 
@@ -252,7 +246,7 @@ post :: ManagedQueue -> VkSubmitInfo -> Prog r ()
 post ManagedQueue{ requestChan } submitInfo =
   writeChan requestChan $ Transaction [Post submitInfo]
 
-postSubmitPresent :: ManagedQueue -> VkSubmitInfo -> ManagedPresentQueue -> (forall r'. Prog r' ()) -> IO () -> Prog r ()
+postSubmitPresent :: ManagedQueue -> VkSubmitInfo -> ManagedPresentQueue -> (forall r. Prog r VkResult) -> (forall r. VkResult -> Prog r ()) -> Prog r' ()
 postSubmitPresent ManagedQueue{ requestChan } submitInfo presentQueue presentAction postPresentAction =
   writeChan requestChan $ Transaction
     [Post submitInfo, Submit, Present presentQueue presentAction postPresentAction]
@@ -267,7 +261,7 @@ submit :: ManagedQueue -> Prog r ()
 submit ManagedQueue{ requestChan } =
   writeChan requestChan $ Transaction [Submit]
 
-submitPresent :: ManagedQueue -> ManagedPresentQueue -> (forall r'. Prog r' ()) -> IO () -> Prog r ()
+submitPresent :: ManagedQueue -> ManagedPresentQueue -> (forall r. Prog r VkResult) -> (forall r. VkResult -> Prog r ()) -> Prog r' ()
 submitPresent ManagedQueue{ requestChan } presentQueue presentAction postPresentAction =
   writeChan requestChan $ Transaction
     [Submit, Present presentQueue presentAction postPresentAction]